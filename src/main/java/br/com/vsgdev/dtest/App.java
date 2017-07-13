16008ceeb18b7d723062877da41f7522a298869c
Stênio Galvão <stenio@infoway-pi.com.br>
2017-07-13 17:55:23 -0300

    danger test

--- src/main/java/br/com/vsgdev/dtest/App.java
+++ src/main/java/br/com/vsgdev/dtest/App.java
@@ -10,6 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "asd!" );
+        System.out.println( "asd!" );
+        System.out.println( "asd!" );
         
     }
 }
